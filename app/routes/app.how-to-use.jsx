f7d95b96dd214c8a3f3ad59e5f1efc633463d47c
Harsh Ajudia <dev@growthdigital.com.au>
2026-01-30 10:02:06 +0530

    Updated How to use route

--- app/routes/app.how-to-use.jsx
+++ app/routes/app.how-to-use.jsx
@@ -4,7 +4,7 @@ export default function HowToUse() {
             <s-box paddingBlockStart="large" paddingBlockEnd="large">
                 <s-section heading="How To Use">
                     <s-paragraph>
-                        To learn more about how this app works, <i>click here</i> to download the PDF.
+                        To learn more about how this app works, <a href="/gd-wholesale-pro-how-to-use.pdf" download target="_blank">click here</a> to download the PDF.
                     </s-paragraph>
                 </s-section>
             </s-box>
